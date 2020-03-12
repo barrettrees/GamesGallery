aeee1e4a1ab2c484ff25b4db59bbecc573f46fbb
barrettrees <barrettrees@gmail.com>
2020-03-12 16:48:34 -0700

    Testing first changes for 2020

--- data.js
+++ data.js
@@ -1,234 +1,86 @@
 
 var games = [ 
 	{
-		name : "untitled",
+		name : "Radio Fall",
 		platform : "Twine",
-		dir : "SebastianMoore",
-		authors : [ "Sebastian Moore" ],
-		showSourceDownload : false,
-		description : ""
+		dir : "RadioFall",
+		authors : [ "Michelle Lytle" ],
+		showSourceDownload : true,
+		description : "<p>Placeholder description.</p>"
 	},{
-		name : "Bitter Pill",
+		name : "In an Orderly Fashion",
 		platform : "Twine",
-		dir : "BitterPill",
-		authors : [ "Margaret Patrick" ],
-		showSourceDownload : false,
-		description : ""
+		dir : "InAnOrderlyFashion",
+		authors : [ "Oliver Berggren" ],
+		showSourceDownload : true,
+		description : "<p>Placeholder description.</p>"
 	},{
-		name : "Friction",
+		name : "Ezelia's Elaborate Escape",
 		platform : "Twine",
-		dir : "Friction",
-		authors : [ "Ardent Eliot Reinhard" ],
+		dir : "EzeliasElaborateEscape",
+		authors : [ "Kenneth Blum" ],
 		showSourceDownload : true,
-		description : ""
+		description : "<p>Placeholder description.</p>"
 	},{
-		name : "Eyes",
+		name : "Meet Me Halfway",
+		platform : "Analog",
+		dir : "MeetMeHalfway",
+		authors : [ "Ashley Lu", "Isabelle Wong" ],
+		showSourceDownload : false,
+		description : "<p>Placeholder description.</p>"
+	},{
+		name : "Social Climber",
+		platform : "Analog",
+		dir : "SocialClimber",
+		authors : [ "Mariel Folkerts", "Lachlan Diaz" ],
+		showSourceDownload : false,
+		description : "<p>Placeholder description.</p>"
+	},{
+		name : "Finals Frenzy",
+		platform : "Analog",
+		dir : "FinalsFrenzy",
+		authors : [ "Maria Becerra", "Ivan Martinez-Arias" ],
+		showSourceDownload : false,
+		description : "<p>Placeholder description.</p>"
+	},{
+		name : "Queen of the Oils",
+		platform : "Analog",
+		dir : "QueenOfTheOils",
+		authors : [ "Sam Feng", "Jose Salas" ],
+		showSourceDownload : false,
+		description : "<p>Placeholder description.</p>"
+	},{
+		name : "Project Love",
 		platform : "Twine",
-		dir : "Eyes",
-		authors : [ "Cam Miller" ],
+		dir : "ProjectLove",
+		authors : [ "Sam Feng" ],
 		showSourceDownload : true,
-		description : ""
-	},{
-		name : "Super Space Dance Battle Deluxe",
-		platform : "Construct",
-		dir : "SuperSpaceDanceBattleDeluxe",
-		authors : [ "Kyle Fegan" ],
-		showSourceDownload : false,
-		description : ""
-	},{
-		name : "Yami",
-		platform : "Construct",
-		dir : "Yami",
-		authors : [ "Kan Wen" ],
-		showSourceDownload : false,
-		description : ""
-	},{
-		name : "Snungeon",
-		platform : "Construct",
-		dir : "Snungeon",
-		authors : [ "Ardent Eliot Reinhard", "Cam Miller", "Cecil Choi" ],
-		showSourceDownload : true,
-		description : "<p>It's a snail dungeon! A goofy, lighthearted dungeon-crawler about a cute, queer snail hero (\"snero\") in shorts and a t-shirt. Designed to be a friendly, easy to play, and short but complete experience. Featuring all original pixelart, chiptune-inspired themes, and voiced sound effects. Inspired by <i>The Legend of Zelda</i>.</p>"
-	},{
-		name : "Janitor Simulator",
-		platform : "Construct",
-		dir : "JanitorSimulator",
-		authors : [ "Ali Zeynalov" ],
-		showSourceDownload : false,
-		description : "<p>Work your shift as a janitor in a hotel lobby. Clean visitor’s footprints, pick up the trash that they throw on the ground, and water plants with a unique isometric view and original art.</p><p>Inspired by <i>Viscera Cleanup Detail</i> and <i>Two Point Hospital</i>.</p>"
-	},{
-		name : "A Dog at the Beach",
-		platform : "GameMaker Studio",
-		dir : "ADogAtTheBeach",
-		authors : [ "Georgio Klironomos", "Terrence Dubois", "Milo Lynch" ],
-		showSourceDownload : false,
-		description : ""
-	},{
-		name : "Stretched Thin",
-		platform : "Construct",
-		dir : "StretchedThin",
-		authors : [ "Lauren Fowlkes" ],
-		showSourceDownload : false,
-		description : "<p>A simple simulation roleplay game in which you play as a single mother working a minimum wage office job, trying to make ends meet while keeping her two sons happy and healthy.</p><p>Hint: Going to work subtracts 2 health and happiness from each family member, as this will take them past lunch and dinner time and they’ll be hungry and tired. If at any point a family member runs out of health or happiness, or you run out of money, you lose the game.</p><p>Credits: <ul><li><a href='https://ansimuz.itch.io/hazy-urban-landscape'>Hazy urban landscape background</a> by ansimuz</li><li><a href='https://sicklyseraph.itch.io/jeremy-and-martha'>Character sprites</a> by sicklyseraph</li><li><a href='https://www.playonloop.com/2014-music-loops/sacred-temple/'>Sacred Temple music</a></li></ul></p>"
-	},{
-		name : "Instrumental",
-		platform : "Construct",
-		dir : "Instrumental",
-		authors : [ "Jimmy Lujan", "Laura Britt", "Gabrielle Serna" ],
-		showSourceDownload : true,
-		description : "<p>Note: Each level has 9 acorns/geodes/leafs that must be collected along with 3 notes (pressing the A, W, and S keys in the correct order) to go on to the next level. The 3 notes must be played on the top of the colored rocks for your instrument to be \"blessed\" and the door to the next level to open.</p>"
-	},{
-		name : "UGLY",
-		platform : "Construct",
-		dir : "UGLY",
-		authors : [ "Margaret Patrick", "Austin Brägger", "Shun Xu" ],
-		showSourceDownload : false,
-		description : ""
-	},{
-		name : "Roach Redemption",
-		platform : "Construct",
-		dir : "RoachRedemption",
-		authors : [ "Htoo Paing" ],
-		showSourceDownload : false,
-		description : ""
-	},{
-		name : "Some Assembly Required​",
-		platform : "Construct",
-		dir : "SomeAssemblyRequired",
-		authors : [ "Carlos Cisneros", "Cedric Tomaschke" ],
-		showSourceDownload : true,
-		description : ""
-	},{
-		name : "Critter Xing",
-		platform : "Construct",
-		dir : "CritterXing",
-		authors : [ "Daniel Avery", "Katarina Springer" ],
-		showSourceDownload : false,
-		description : ""
-	},{
-		name : "Fremont",
-		platform : "Twine",
-		dir : "Fremont",
-		authors : [ "Katie Moses" ],
-		showSourceDownload : false,
-		description : ""
-	},{
-		name : "We could have made them look like anything, but we made them look like us",
-		platform : "Twine",
-		dir : "WeCouldHaveMadeThemLookLikeAnything",
-		authors : [ "Georgio Klironomos" ],
-		showSourceDownload : true,
-		description : ""
+		description : "<p>Placeholder description.</p>"
 	}
 ];
 
 var authors = [ 
 	{
-		name : "Ahri Zheng",
-		instagram : "ahri.cc"
+		name : "Michelle Lytle"
 	},{
-		name : "Alex Lau"
+		name : "Oliver Berggren"
 	},{
-		name : "Ali Zeynalov",
-		website : "ali.zeynalov.com"
+		name : "Kenneth Blum"
 	},{
-		name : "Allen Aboytes"
+		name : "Ashley Lu"
 	},{
-		name : "Ardent Eliot Reinhard",
-		itch : "trophyhusban",
-		twitter : "trophyhusban"
+		name : "Isabelle Wong"
 	},{
-		name : "Ash O'Farrell"
+		name : "Mariel Folkerts"
 	},{
-		name : "Austin Brägger"
+		name : "Lachlan Diaz"
 	},{
-		name : "Ben Rowland"
+		name : "Maria Becerra"
 	},{
-		name : "Brady Moore",
-		itch : "kittynugget"
+		name : "Ivan Martinez-Arias"
 	},{
-		name : "Brandon Erickson",
-		email : "bjericks@ucsc.edu"
+		name : "Sam Feng"
 	},{
-		name : "Brandon Leung",
-		instagram : "baoeung"
-	},{
-		name : "Cam Miller",
-		website : "sashimi.space",
-		itch : "camsh",
-		twitter : "CammySashimi"
-	},{
-		name : "Carlos Cisneros"
-	},{
-		name : "Cecil Choi",
-		itch : "ceec",
-		twitter : "cerealceec",
-		instagram : "cerealceec"
-	},{
-		name : "Cedric Tomaschke"
-	},{
-		name : "Charles Advincula"
-	},{
-		name : "Cheryl Kulkarni"
-	},{
-		name : "Cole Watts"
-	},{
-		name : "Colin O’Rourke"
-	},{
-		name : "Daniel Aughenbaugh"
-	},{
-		name : "Daniel Liao"
-	},{
-		name : "Emily Ye",
-		instagram : "sour_fruit"
-	},{
-		name : "Eric Huang"
-	},{
-		name : "Fion Kwok"
-	},{
-		name : "Gabrielle Serna"
-	},{
-		name : "Georgio Klironomos",
-		itch : "recyclist"
-	},{
-		name : "Isha Sethi "
-	},{
-		name : "Jimmy Lujan",
-		twitter : "jjlp98"
-	},{
-		name : "Joel M."
-	},{
-		name : "Katie Moses"
-	},{
-		name : "Ki"
-	},{
-		name : "Laura Britt",
-		instagram : "puzzleleafs"
-	},{
-		name : "Lauren Fowlkes",
-		instagram : "laurenfowlkes"
-	},{
-		name : "Margaret Patrick"
-	},{
-		name : "Milo Lynch"
-	},{
-		name : "Quan Gao",
-		linkedin : "https://www.linkedin.com/in/quan-gao-b40108106/"
-	},{
-		name : "Shun Xu"
-	},{
-		name : "Simran Gunsi",
-		twitter : "simrxnk",
-		instagram : "simrxnk"
-	},{
-		name : "Steven Huang"
-	},{
-		name : "Teng Ma"
-	},{
-		name : "Terence So"
-	},{
-		name : "Terrence Dubois"
-	},{
-		name : "Yuda Kuang <--check"
+		name : "Jose Salas"
 	}
 ];
