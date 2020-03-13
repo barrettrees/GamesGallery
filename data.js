dc13a71977a7a402db7874cfaa4c7431053a461e
unknown <barrettrees@gmail.com>
2020-03-13 09:31:40 -0700

    Updated games, first pass

--- data.js
+++ data.js
@@ -6,56 +6,56 @@ var games = [
 		dir : "RadioFall",
 		authors : [ "Michelle Lytle" ],
 		showSourceDownload : true,
-		description : "<p>Placeholder description.</p>"
+		description : "<p>A link-based game about late afternoon naps, communication, and you!</p>"
 	},{
 		name : "In an Orderly Fashion",
 		platform : "Twine",
 		dir : "InAnOrderlyFashion",
 		authors : [ "Oliver Berggren" ],
 		showSourceDownload : true,
-		description : "<p>Placeholder description.</p>"
+		description : "<p>'In an Orderly Fashion' is Mexican-American's immigration simulation game. You, the player, are a law-abiding citizen trying to cross the border between Tijuana and San Diego. Gameplay is random, and much of it's out of your control: it changes depending on the guards' racism, the border's security level, your anxiety, your ethnicity, your appearance, and so forth. You have a variety of possible responses, based on constitutional rights, your self-respect, your personal background, and more. All of these responses may or may not help you cross. As you talk to immigration authorities, check your privilege: it will open and close certain doors.</p>"
 	},{
-		name : "Ezelia's Elaborate Escape",
+		name : "Ezelia\'s Elaborate Escape",
 		platform : "Twine",
 		dir : "EzeliasElaborateEscape",
 		authors : [ "Kenneth Blum" ],
 		showSourceDownload : true,
-		description : "<p>Placeholder description.</p>"
+		description : "<p>Ezelia , a girl with powers she doesn\'t know how to harness, has been captured by the evil wizard Dramodus. She must escape with the help of other prisoners, and seek out her parents.</p>"
 	},{
 		name : "Meet Me Halfway",
 		platform : "Analog",
 		dir : "MeetMeHalfway",
 		authors : [ "Ashley Lu", "Isabelle Wong" ],
 		showSourceDownload : false,
-		description : "<p>Placeholder description.</p>"
+		description : "<p>Our game aims to present the world in the 19th century, a time before automobiles and airplanes which are forms of fast travelling. It has an emphasis on travelling across the world and encountering inconveniences before reaching your destination as well as connections with others during this time.</p>"
 	},{
 		name : "Social Climber",
 		platform : "Analog",
 		dir : "SocialClimber",
 		authors : [ "Mariel Folkerts", "Lachlan Diaz" ],
 		showSourceDownload : false,
-		description : "<p>Placeholder description.</p>"
+		description : "<p>Social Climber alludes to the current social climate that we face in today’s world which is severely underrepresented in the medium of games. Social media is such a big part of everyday life and is becoming an increasing symbol of social status, particularly in regards to someone’s \“follower count.\” The more they have, the more successful or popular they are considered. </p>"
 	},{
 		name : "Finals Frenzy",
 		platform : "Analog",
 		dir : "FinalsFrenzy",
 		authors : [ "Maria Becerra", "Ivan Martinez-Arias" ],
 		showSourceDownload : false,
-		description : "<p>Placeholder description.</p>"
+		description : "<p>Fin(A+)ls Frenzy is a 3-4 player game where each player is a student with a different major, frantically trying to use their time wisely to study for all their finals. Not only that, but seats at the library can be taken at any time! As a student, you must take the opportunities offered to you and spread your time and energy among all your classes in order to pass them all!</p>"
 	},{
 		name : "Queen of the Oils",
 		platform : "Analog",
 		dir : "QueenOfTheOils",
 		authors : [ "Sam Feng", "Jose Salas" ],
 		showSourceDownload : false,
-		description : "<p>Placeholder description.</p>"
+		description : "<p>Our game presents a world where big companies from different countries battling for lands to pump oils. We want to use it represent the harsh reality of the oil war that is happening in our world today.</p>"
 	},{
-		name : "Project Love",
+		name : "The Perfect Gift",
 		platform : "Twine",
 		dir : "ProjectLove",
 		authors : [ "Sam Feng" ],
 		showSourceDownload : true,
-		description : "<p>Placeholder description.</p>"
+		description : "<p>A Twine adventure game where you try to walk to different places and collect random items to make a gift for your crush.</p>"
 	}
 ];
 
