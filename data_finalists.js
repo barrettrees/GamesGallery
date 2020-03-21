d436d4d37ea64fa030df51116a1b0b72d97ae118
barrettrees <barrettrees@gmail.com>
2020-03-21 12:48:57 -0700

    Added Finalists page and randomly shuffling game order.

---
+++ data_finalists.js
@@ -0,0 +1,224 @@
+
+var games = [ 
+	{
+		name : "Radio Fall",
+		platform : "Twine",
+		dir : "RadioFall",
+		authors : [ "Michelle Lytle" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>A game about communication and loneliness.</p>"
+	},{
+		name : "In an Orderly Fashion",
+		platform : "Twine",
+		dir : "InAnOrderlyFashion",
+		authors : [ "Oliver Berggren" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>\'In an Orderly Fashion\' is Mexican-American's immigration simulation game. You, the player, are a law-abiding citizen trying to cross the border between Tijuana and San Diego. Gameplay is random, and much of it's out of your control: it changes depending on the guards\' racism, the border's security level, your anxiety, your ethnicity, your appearance, and so forth. You have a variety of possible responses, based on constitutional rights, your self-respect, your personal background, and more. All of these responses may or may not help you cross. As you talk to immigration authorities, check your privilege: it will open and close certain doors.</p>"
+	},{
+
+		name : "Meet Me Halfway",
+		platform : "Analog",
+		dir : "MeetMeHalfway",
+		authors : [ "Ashley Lu", "Isabelle Wong" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>It's the 19th century and there are no airplanes and automobiles. It is hard to meet people across the world. This game is about strengthening connections through less convenient means. The goal of the game is to not be the last player to travel to the specified city/country. Made for 2-4 players.</p>"
+	},{
+		name : "FIN(A+)LS FRENZY",
+		platform : "Analog",
+		dir : "FinalsFrenzy",
+		authors : [ "Fernanda Becerra", "Ivan Martinez-Arias" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>FIN(A+)LS FRENZY is a 3-9 player game where each player is a student with a different major, frantically trying to use their time wisely to study for all their finals. Not only that, but seats in the library can be taken at any time! As a student, you must take the opportunities offered to you and spread your time and energy evenly among all you classes in order to pass them all!</p>"
+	},{
+		name : "Queen of the Oils",
+		platform : "Analog",
+		dir : "QueenOfTheOils",
+		authors : [ "Sam Feng", "Jose Salas" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>Queen of the Oils is a strategic, fast-paced, tabletop game delving into the management of land resources in the competition for dominance over the oil industry.</p>"
+	},{
+		name : "The Perfect Gift",
+		platform : "Twine",
+		dir : "ProjectLove",
+		authors : [ "Sam Feng" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>You are a normal college student, and you have to go around town to find the perfect birthday gift for the girl you crushed on...</p>"
+	},{
+		name : "A Long Way Home",
+		platform : "Bitsy",
+		dir : "ALongWayHome",
+		authors : [ "Alicia Zhen", "Brandon Kharaka", "Joann Marie Wong" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>A game about a lost cat who must find their way home.</p>"
+	},{
+		name : "Birds aren\'t Real",
+		platform : "Godot",
+		dir : "BirdsArentReal",
+		authors : [ "Jameson Danning", "Colin Watterson" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>Control a \'bird\' (government spy drone) and listen in on your fellow citizen's private conversations! Explore the park, listen carefully, and report any suspicious behavior!<p>"
+	},{
+		name : "Le Correspondant Perdu",
+		platform : "Twine",
+		dir : "LeCorrespondantPerdu",
+		authors : [ "Matthew Boisvert" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>\"Le Correspondant Perdu\" is a mystery game that presents the fiction of an American officer-worker, John, whose French penpal Pierre goes missing in the year 1985. Because of the close relationship that John had with Pierre, he decides that he needs to figure out what happened to Pierre in order to try and save him from the seemingly dangerous situation that he is in. What John does not know is that the U.S.S.R. was really interested in Pierre for his calligraphy skills.<\p>"
+	},{
+		name : "Distance",
+		platform : "Twine",
+		dir : "Distance",
+		authors : [ "Nathann Latimore" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>You\'ve stumbled across a dirty photograph of you and your dad. As you try to scratch the markings away, you realize that you\'re having trouble remembering his face. This should be easy, right? The funeral was a while ago, but, if anything, this should be something simple to remember. In \"Distance\", a text-based hyper-link game, you traverse the interconnected pathways of your memory by uncovering moments that shine a light on special times with your family, all in an attempt to rediscover your father\'s smile.</p>"
+	},{
+		name : "Hop Hop Donut",
+		platform : "Godot",
+		dir : "HopHopDonut",
+		authors : [ "Sam Feng", "Jose Salas" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>Hop Hop Donut is a pixel 2D platformer where the player is the main character, Donut, a Welsh Corgi in search of their human owner who has been kidnapped by a band of house cats.</p>"
+	},{
+		name : "Discovery",
+		platform : "Godot",
+		dir : "Discovery",
+		authors : [ "Nathann Latimore", "Harrison Kurtz" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>Discovery is a game about the pressure of time and life experience. In the vein of classic 2D-space-shooter games, you pilot a ship through an asteroid field, avoiding an expanding supernova. Scattered throughout space are glowing orbs which contain moments: memories and bits of conversation. How many moments can you experience before you meet your end?</p>"
+	},{
+		name : "One Last Chance",
+		platform : "",
+		dir : "OneLastChance",
+		authors : [ "Carl Erez", "Lucio Espinoza", "Mark Medved" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>A reskin of Frogger in which you navigate the difficult conversation of a father trying to reconnect with his estranged son.</p>"
+	},{
+		name : "Calming Pressures",
+		platform : "Bitsy",
+		dir : "CalmingPressures",
+		authors : [ "Niccolo Abate", "Tate Pieper" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>In this game about coming to terms with your life / giving up on dreams and being content, enter the world of a little optimistic fish living in a fishtank, but thinking he is living in the ocean. Explore the pretty, interesting world of ocean/fishtank and unfold the story in your own personal way, seeing how the space changes as well.</p>"
+	},{
+		name : "Mindscape",
+		platform : "Bitsy",
+		dir : "Mindscape",
+		authors : [ "Enrico Widodo", "Jess Wake" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>Mindscape is a game about exploring the worlds that humans create within our minds. It is also about coping with the nature of fiction, and attachment to characters and events who aren't \"real.\"</p>"
+	}
+];
+
+var authors = [ 
+	{
+		name : "Alicia Zhen",
+		itch : "https://sad-boi-hours.itch.io/",
+		twitter : "@yukidesoo",
+		instagram : "@wallflower.png & @yukidesoo",
+		linkedin : "https://www.linkedin.com/in/alicia-zhen-466b8119b/",
+		email : "zhenalicia@gmail.com"
+	},{
+		name : "Ashley Lu"
+	},{
+		name : "Brandon Kharaka"
+	},{
+		name : "Carl Erez"
+	},{
+		name : "Changlin Yang"
+	},{
+		name : "Colin Watterson",
+		itch : "https://colinw.itch.io/",
+		linkedin : "https://www.linkedin.com/in/colin-watterson-63a4391a1/",
+		email : "ctwatter@ucsc.edu"
+	},{
+		name : "Enrico Widodo"
+	},{
+		name : "Fernanda Becerra",
+		instagram : "fernanda.b13",
+		email : "mfbecerr@ucsc.edu"
+	},{
+		name : "Harrison Kurtz"
+	},{
+		name : "Isaac Tremble-Pederson"
+	},{
+		name : "Isabelle Wong"
+	},{
+		name : "Ivan Martinez-Arias",
+		linkedin : "https://www.linkedin.com/in/ivan-martinez-arias-2898a0192/",
+		email : "martinez14ivan0427@gmail.com"
+	},{
+		name : "Jameson Danning",
+		website : "https://jdanning.myportfolio.com/",
+		itch : "https://jamesondanning.itch.io/",
+		instagram : "@bigboijamo",
+		linkedin : "https://www.linkedin.com/in/jameson-danning-418440193/",
+		email : "jdanning@ucsc.edu"
+	},{
+		name : "Jess Wake"
+	},{
+		name : "Joann Marie Wong"
+	},{
+		name : "Jose Salas"
+	},{
+		name : "Justin Chang"
+	},{
+		name : "Kenneth Blum"
+	},{
+		name : "Lachlan Diaz"
+	},{
+		name : "Lucio Espinoza"
+	},{
+		name : "Maria Becerra"
+	},{
+		name : "Mariel Folkerts"
+	},{
+		name : "Mark Medved"
+	},{
+		name : "Matthew Boisvert",
+		linkedin : "https://www.linkedin.com/in/matthew-boisvert-aa5732158/",
+		email : "mboisver@ucsc.edu"
+	},{
+		name : "Matthew Boisvert"
+	},{
+		name : "Meichi Chen",
+		email : "mchen99@ucsc.edu"
+	},{
+		name : "Michelle Lytle",
+		email : "mnlytle@ucsc.edu"
+	},{
+		name : "Mingun Cho"
+	},{
+		name : "Nathan Ma"
+	},{
+		name : "Nathann Latimore",
+		twitter : "@Sonar_101",
+		instagram : "@sonar101",
+		youtube : "https://www.youtube.com/channel/UCLAU14RVIQezSmvyAwXZRxA",
+		email : "N.Latimore1013@gmail.com"
+	},{
+		name : "Niccolo Abate"
+	},{
+		name : "Oliver Berggren"
+	},{
+		name : "Sam Feng"
+	},{
+		name : "Tate Pieper"
+	},{
+		name : "Zhifeng Lu"
+	}
+];
--- homepage.js
+++ homepage.js
@@ -15,6 +15,7 @@
     in id "game-container"
  */
 function makeGameCards () {
+	games = shuffle(games)
 
 	// For each game 
 	for (let game of games) {
@@ -62,4 +63,22 @@ $(window).on ("load", function () {
 
 });
 
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+};
 
