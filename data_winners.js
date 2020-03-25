1de0bc911b816a6866a85d0cbe8829de59291f8d
barrettrees <barrettrees@gmail.com>
2020-03-24 17:21:36 -0700

    added winners

--- data_winners.js
+++ data_winners.js
@@ -16,31 +16,7 @@ var games = [
 		showPlayButton : true,
 		showSourceDownload : true,
 		description : "<p>The  border-crossing experience from Mexico to America.</p>"
-	},{
-		name : "Meet Me Halfway",
-		platform : "Analog",
-		dir : "MeetMeHalfway",
-		authors : [ "Ashley Lu", "Isabelle Wong" ],
-		showPlayButton : true,
-		showSourceDownload : true,
-		description : "<p>It's the 19th century and there are no airplanes and automobiles. It is hard to meet people across the world. This game is about strengthening connections through less convenient means. The goal of the game is to not be the last player to travel to the specified city/country. Made for 2-4 players.</p>"
-	},{
-		name : "FIN(A+)LS FRENZY",
-		platform : "Analog",
-		dir : "FinalsFrenzy",
-		authors : [ "Fernanda Becerra", "Ivan Martinez-Arias" ],
-		showPlayButton : true,
-		showSourceDownload : true,
-		description : "<p>FIN(A+)LS FRENZY is a 3-9 player game where each player is a student with a different major, frantically trying to use their time wisely to study for all their finals. Not only that, but seats in the library can be taken at any time! As a student, you must take the opportunities offered to you and spread your time and energy evenly among all you classes in order to pass them all!</p>"
-	},{
-		name : "Queen of the Oils",
-		platform : "Analog",
-		dir : "QueenOfTheOils",
-		authors : [ "Sam Feng", "Jose Salas" ],
-		showPlayButton : true,
-		showSourceDownload : true,
-		description : "<p>Queen of the Oils is a strategic, fast paced, table top game delving into the management of land resources in the competition for dominance over the oil industry.</p>"
-	},{
+	},{	
 		name : "The Perfect Gift",
 		platform : "Twine",
 		dir : "ProjectLove",
@@ -57,30 +33,6 @@ var games = [
 		showSourceDownload : true,
 		description : "<p>Our game is about a lost cat who must find their way home. The story is that you’re an outdoor can who was let out of the house to roam one evening. Upon leaving the house, the cat finds a butterfly and starts chasing it all around the neighborhood. Eventually, the cat loses its way home and must find a way back.</p>"
 	},{
-		name : "Birds Aren\'t Real",
-		platform : "Godot",
-		dir : "BirdsArentReal",
-		authors : [ "Jameson Danning", "Colin Watterson" ],
-		showPlayButton : true,
-		showSourceDownload : true,
-		description : "<p>Control a 'bird' (government spy drone) and listen in on your fellow citizen's private conversations! Explore the park, listen carefully, and report any suspicious behavior!<p>"
-	},{
-		name : "Le Correspondant Perdu",
-		platform : "Twine",
-		dir : "LeCorrespondantPerdu",
-		authors : [ "Matthew Boisvert" ],
-		showPlayButton : true,
-		showSourceDownload : true,
-		description : "<p>\"Le Correspondant Perdu\" is a mystery game that presents the fiction of an American officer-worker, John, whose French penpal Pierre goes missing in the year 1985. Because of the close relationship that John had with Pierre, he decides that he needs to figure out what happened to Pierre in order to try and save him from the seemingly dangerous situation that he is in. What John does not know is that the U.S.S.R. was really interested in Pierre for his calligraphy skills.<\p>"
-	},{
-		name : "Distance",
-		platform : "Twine",
-		dir : "Distance",
-		authors : [ "Nathann Latimore" ],
-		showPlayButton : true,
-		showSourceDownload : true,
-		description : "<p>In Distance, a link based game created by Nathann Latimore, the player gets to explore how far love travels by literally traversing the interconnected memories of their character, who has, unfortunately, had something fade from her mind that she thought she could never forget: the smiling face of her father, who has sadly passed away.</p>"
-	},{
 		name : "Hop Hop Donut",
 		platform : "Godot",
 		dir : "HopHopDonut",
@@ -89,22 +41,6 @@ var games = [
 		showSourceDownload : false,
 		description : "<p>Donut the corgi had his owner kidnapped by an evil band of cats who envy the peace between humans and dogs, but luckily they aren't too bright. So, strap on your balloons with Donut and take to the skies, battling enemy critters in search of your beloved owner and find who is behind this nasty plot.</p>"
 	},{
-		name : "Discovery",
-		platform : "Godot",
-		dir : "Discovery",
-		authors : [ "Nathann Latimore", "Harrison Kurtz" ],
-		showPlayButton : true,
-		showSourceDownload : true,
-		description : "<p>Discovery is a game about the pressure of time and life experience. In the vein of classic 2D-space-shooter games, you pilot a ship through an asteroid field, avoiding an expanding supernova. Scattered throughout space are glowing orbs which contain moments: memories and bits of conversation. How many moments can you experience before you meet your end?</p>"
-	},{
-		name : "One Last Chance",
-		platform : "",
-		dir : "OneLastChance",
-		authors : [ "Carl Erez", "Lucio Espinoza", "Mark Medved" ],
-		showPlayButton : true,
-		showSourceDownload : true,
-		description : "<p>A reskin of Frogger in which you navigate the difficult conversation of a father trying to reconnect with his estranged son.</p>"
-	},{
 		name : "Calming Pressures",
 		platform : "Bitsy",
 		dir : "CalmingPressures",
@@ -112,14 +48,6 @@ var games = [
 		showPlayButton : true,
 		showSourceDownload : true,
 		description : "<p>Calming Pressures is an atmospheric story game about a young, ambitious fish, excited to find his dreams in the endless oceans. But when he ventures out and explores his world, he finds it to be small and caged in glass. He is in a fishtank.</p>"
-	},{
-		name : "Mindscape",
-		platform : "Bitsy",
-		dir : "Mindscape",
-		authors : [ "Enrico Widodo", "Jess Wake" ],
-		showPlayButton : true,
-		showSourceDownload : true,
-		description : "<p>A Game About Fiction</p>"
 	}
 ];
 
