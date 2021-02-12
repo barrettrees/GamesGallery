f5b72dbb158f386aaa053b607eae139fcbfd1fe3
Barrett Anderson <barrettrees@gmail.com>
2021-02-12 13:20:40 -0800

    uploading the gallery

--- data.js
+++ data.js
@@ -143,7 +143,7 @@ var games = [
 		authors : [ "Sam Feng", "Jose Salas" ],
 		showPlayButton : true,
 		showSourceDownload : false,
-		description : "<p>Donut the corgi had his owner kidnapped by an evil band of cats who envy the peace between humans and dogs, but luckily they aren't too bright. So, strap on your balloons with Donut and take to the skies, battling enemy critters in search of your beloved owner and find who is behind this nasty plot.</p>"
+		description : "<p>Donut the corgi had his owner kidnapped by an evil band of cats who envy the peace between humans and dogs, but luckily they aren�t too bright. So, strap on your balloons with Donut and take to the skies, battling enemy critters in search of your beloved owner and find who is behind this nasty plot.</p>"
 	},{
 		name : "The Hangover Night",
 		platform : "Bitsy",
@@ -200,6 +200,142 @@ var games = [
 		showPlayButton : true,
 		showSourceDownload : true,
 		description : "<p> Dead A Live is a horror, thriller, RPG game made with Godot. The player plays as a janitor who is working within a normal hospital at night. However, the janitor has accidentally taken some hallucinogens and starts seeing the hospital as a very frightening atmosphere. Even though the player perceives the visuals as scary, in reality its not. The player must navigate through a series of hallways and rooms while avoiding monsters to find an exit point. The player can interact with doors and has a rechargeable flashlight to observe their surroundings better. </p>"
+	},{
+		name : "untitled",
+		platform : "Twine",
+		dir : "SebastianMoore",
+		authors : [ "Sebastian Moore" ],
+		showPlayButton : true,
+		showSourceDownload : false,
+		description : ""
+	},{
+		name : "Bitter Pill",
+		platform : "Twine",
+		dir : "BitterPill",
+		authors : [ "Margaret Patrick" ],
+		showPlayButton : true,
+		showSourceDownload : false,
+		description : ""
+	},{
+		name : "Friction",
+		platform : "Twine",
+		dir : "Friction",
+		authors : [ "Ardent Eliot Reinhard" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : ""
+	},{
+		name : "Eyes",
+		platform : "Twine",
+		dir : "Eyes",
+		authors : [ "Cam Miller" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : ""
+	},{
+		name : "Super Space Dance Battle Deluxe",
+		platform : "Construct",
+		dir : "SuperSpaceDanceBattleDeluxe",
+		authors : [ "Kyle Fegan" ],
+		showPlayButton : true,
+		showSourceDownload : false,
+		description : ""
+	},{
+		name : "Yami",
+		platform : "Construct",
+		dir : "Yami",
+		authors : [ "Kan Wen" ],
+		showPlayButton : true,
+		showSourceDownload : false,
+		description : ""
+	},{
+		name : "Snungeon",
+		platform : "Construct",
+		dir : "Snungeon",
+		authors : [ "Ardent Eliot Reinhard", "Cam Miller", "Cecil Choi" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>It's a snail dungeon! A goofy, lighthearted dungeon-crawler about a cute, queer snail hero (\"snero\") in shorts and a t-shirt. Designed to be a friendly, easy to play, and short but complete experience. Featuring all original pixelart, chiptune-inspired themes, and voiced sound effects. Inspired by <i>The Legend of Zelda</i>.</p>"
+	},{
+		name : "Janitor Simulator",
+		platform : "Construct",
+		dir : "JanitorSimulator",
+		authors : [ "Ali Zeynalov" ],
+		showPlayButton : true,
+		showSourceDownload : false,
+		description : "<p>Work your shift as a janitor in a hotel lobby. Clean visitor’s footprints, pick up the trash that they throw on the ground, and water plants with a unique isometric view and original art.</p><p>Inspired by <i>Viscera Cleanup Detail</i> and <i>Two Point Hospital</i>.</p>"
+	},{
+		name : "A Dog at the Beach",
+		platform : "GameMaker Studio",
+		dir : "ADogAtTheBeach",
+		authors : [ "Georgio Klironomos", "Terrence Dubois", "Milo Lynch" ],
+		showPlayButton : true,
+		showSourceDownload : false,
+		description : ""
+	},{
+		name : "Stretched Thin",
+		platform : "Construct",
+		dir : "StretchedThin",
+		authors : [ "Lauren Fowlkes" ],
+		showPlayButton : true,
+		showSourceDownload : false,
+		description : "<p>A simple simulation roleplay game in which you play as a single mother working a minimum wage office job, trying to make ends meet while keeping her two sons happy and healthy.</p><p>Hint: Going to work subtracts 2 health and happiness from each family member, as this will take them past lunch and dinner time and they’ll be hungry and tired. If at any point a family member runs out of health or happiness, or you run out of money, you lose the game.</p><p>Credits: <ul><li><a href='https://ansimuz.itch.io/hazy-urban-landscape'>Hazy urban landscape background</a> by ansimuz</li><li><a href='https://sicklyseraph.itch.io/jeremy-and-martha'>Character sprites</a> by sicklyseraph</li><li><a href='https://www.playonloop.com/2014-music-loops/sacred-temple/'>Sacred Temple music</a></li></ul></p>"
+	},{
+		name : "Instrumental",
+		platform : "Construct",
+		dir : "Instrumental",
+		authors : [ "Jimmy Lujan", "Laura Britt", "Gabrielle Serna" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>Note: Each level has 9 acorns/geodes/leafs that must be collected along with 3 notes (pressing the A, W, and S keys in the correct order) to go on to the next level. The 3 notes must be played on the top of the colored rocks for your instrument to be \"blessed\" and the door to the next level to open.</p>"
+	},{
+		name : "UGLY",
+		platform : "Construct",
+		dir : "UGLY",
+		authors : [ "Margaret Patrick", "Austin Brägger", "Shun Xu" ],
+		showPlayButton : true,
+		showSourceDownload : false,
+		description : ""
+	},{
+		name : "Roach Redemption",
+		platform : "Construct",
+		dir : "RoachRedemption",
+		authors : [ "Htoo Paing" ],
+		showPlayButton : true,
+		showSourceDownload : false,
+		description : ""
+	},{
+		name : "Some Assembly Required​",
+		platform : "Construct",
+		dir : "SomeAssemblyRequired",
+		authors : [ "Carlos Cisneros", "Cedric Tomaschke" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : ""
+	},{
+		name : "Critter Xing",
+		platform : "Construct",
+		dir : "CritterXing",
+		authors : [ "Daniel Avery", "Katarina Springer" ],
+		showPlayButton : true,
+		showSourceDownload : false,
+		description : ""
+	},{
+		name : "Fremont",
+		platform : "Twine",
+		dir : "Fremont",
+		authors : [ "Katie Moses" ],
+		showPlayButton : true,
+		showSourceDownload : false,
+		description : ""
+	},{
+		name : "We could have made them look like anything, but we made them look like us",
+		platform : "Twine",
+		dir : "WeCouldHaveMadeThemLookLikeAnything",
+		authors : [ "Georgio Klironomos" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : ""
 	}
 ];
 
@@ -305,5 +441,112 @@ var authors = [
 		name : "Victor Chung"
 	},{
 		name : "Tim D'Amore"
+	},{
+		name : "Ahri Zheng",
+		instagram : "ahri.cc"
+	},{
+		name : "Alex Lau"
+	},{
+		name : "Ali Zeynalov",
+		website : "ali.zeynalov.com"
+	},{
+		name : "Allen Aboytes"
+	},{
+		name : "Ardent Eliot Reinhard",
+		itch : "trophyhusban",
+		twitter : "trophyhusban"
+	},{
+		name : "Ash O'Farrell"
+	},{
+		name : "Austin Brägger"
+	},{
+		name : "Ben Rowland"
+	},{
+		name : "Brady Moore",
+		itch : "kittynugget"
+	},{
+		name : "Brandon Erickson",
+		email : "bjericks@ucsc.edu"
+	},{
+		name : "Brandon Leung",
+		instagram : "baoeung"
+	},{
+		name : "Cam Miller",
+		website : "sashimi.space",
+		itch : "camsh",
+		twitter : "CammySashimi"
+	},{
+		name : "Carlos Cisneros"
+	},{
+		name : "Cecil Choi",
+		itch : "ceec",
+		twitter : "cerealceec",
+		instagram : "cerealceec"
+	},{
+		name : "Cedric Tomaschke"
+	},{
+		name : "Charles Advincula"
+	},{
+		name : "Cheryl Kulkarni"
+	},{
+		name : "Cole Watts"
+	},{
+		name : "Colin O’Rourke"
+	},{
+		name : "Daniel Aughenbaugh"
+	},{
+		name : "Daniel Liao"
+	},{
+		name : "Emily Ye",
+		instagram : "sour_fruit"
+	},{
+		name : "Eric Huang"
+	},{
+		name : "Fion Kwok"
+	},{
+		name : "Gabrielle Serna"
+	},{
+		name : "Georgio Klironomos",
+		itch : "recyclist"
+	},{
+		name : "Isha Sethi "
+	},{
+		name : "Jimmy Lujan",
+		twitter : "jjlp98"
+	},{
+		name : "Joel M."
+	},{
+		name : "Katie Moses"
+	},{
+		name : "Ki"
+	},{
+		name : "Laura Britt",
+		instagram : "puzzleleafs"
+	},{
+		name : "Lauren Fowlkes",
+		instagram : "laurenfowlkes"
+	},{
+		name : "Margaret Patrick"
+	},{
+		name : "Milo Lynch"
+	},{
+		name : "Quan Gao",
+		linkedin : "https://www.linkedin.com/in/quan-gao-b40108106/"
+	},{
+		name : "Shun Xu"
+	},{
+		name : "Simran Gunsi",
+		twitter : "simrxnk",
+		instagram : "simrxnk"
+	},{
+		name : "Steven Huang"
+	},{
+		name : "Teng Ma"
+	},{
+		name : "Terence So"
+	},{
+		name : "Terrence Dubois"
+	},{
+		name : "Yuda Kuang"
 	}
 ];
